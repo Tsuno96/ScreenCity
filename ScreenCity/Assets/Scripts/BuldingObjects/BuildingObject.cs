3ae022188294bbe79e7d6cad6b9a6b68c430704b
valentinLampriere <valentin.lampriere1@gmail.com>
2020-11-20 14:41:52 +0100

    :tomato:  Add proper material to preview objects

--- ScreenCity/Assets/Scripts/BuldingObjects/BuildingObject.cs
+++ ScreenCity/Assets/Scripts/BuldingObjects/BuildingObject.cs
@@ -14,9 +14,23 @@ public abstract class BuildingObject : MonoBehaviour {
         return Quaternion.identity;
     }
 
+    public static void ToFadeMode(Material material) {
+        material.SetOverrideTag("RenderType", "Transparent");
+        material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+        material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+        material.SetInt("_ZWrite", 0);
+        material.DisableKeyword("_ALPHATEST_ON");
+        material.EnableKeyword("_ALPHABLEND_ON");
+        material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+        material.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
+    }
+
     protected void SetTransparent(GameObject g) {
         MeshRenderer mr = g.GetComponent<MeshRenderer>();
         if (mr == null) return;
+
+        BuildingObject.ToFadeMode(mr.material);
+
         Color c = mr.material.color;
         g.GetComponent<MeshRenderer>().material.color = new Color(c.r, c.g, c.b, 0.5f);
     }
