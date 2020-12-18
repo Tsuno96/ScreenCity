3b30f4f1ceb9bb70f2d598e045df8453623909a6
Mathieu <tsunoll96@gmail.com>
2020-12-18 16:53:35 +0100

    Add ApplyMat On Cube

--- ScreenCity/Assets/Scripts/GameModes/Edit_Mode.cs
+++ ScreenCity/Assets/Scripts/GameModes/Edit_Mode.cs
@@ -20,27 +20,37 @@ public class Edit_Mode : Game_Mode {
             return;
         CubeController cc = hit.transform.gameObject.GetComponent<CubeController>();
         Face f = cc.GetFaceWithNormal(hit.normal);
+        if(Input.GetKey(KeyCode.LeftControl))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                cc.ChangeScale(f, -0.25f);
+                return;
+            }
+        }
         if (Input.GetMouseButtonDown(0)) {
             cc.ChangeScale(f, 0.5f);
         } else if (Input.GetMouseButtonDown(1)) {
-            cc.ChangeScale(f, -0.25f);
+           cc.GetComponent<Renderer>().material = MaterialMGR.Instance.currentMaterial;
+            originalMeshColor = MaterialMGR.Instance.currentMaterial.color;
         }
     }
 
     private void UnFocusSelected() {
         if (currentSelected == null) return;
-        currentSelected.GetComponent<MeshRenderer>().material.color = originalMeshColor;
+        currentSelected.GetComponent<Renderer>().material.color = originalMeshColor;
         currentSelected = null;
     }
 
     private void FocusMesh(GameObject g) {
-        MeshRenderer renderer = g.GetComponent<MeshRenderer>();
+        Renderer renderer = g.GetComponent<Renderer>();
         originalMeshColor = renderer.material.color;
+        Debug.Log(originalMeshColor);
         currentSelected = g;
 
         Color hoverColor = new Color(1 - originalMeshColor.r, 1 - originalMeshColor.g, 1 - originalMeshColor.b);
         hoverColor = Color.Lerp(originalMeshColor, hoverColor, 0.33f);
-        g.GetComponent<MeshRenderer>().material.color = hoverColor;
+        g.GetComponent<Renderer>().material.color = hoverColor;
     }
 
     public override void OnCursorRaycast() {
