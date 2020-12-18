9d073e5a151384544f07d5d090aec360951816da
valentinLampriere <valentin.lampriere1@gmail.com>
2020-12-18 14:04:17 +0100

    Change props on click

--- ScreenCity/Assets/Scripts/PropsItem.cs
+++ ScreenCity/Assets/Scripts/PropsItem.cs
@@ -7,6 +7,12 @@ public class PropsItem : MonoBehaviour {
         GameObject _preview = transform.Find("preview").gameObject;
         GameObject g = Instantiate(_preview, GameObject.Find("Buildings").transform);
         g.transform.localScale = new Vector3(1, 1, 1);
+
+        GameObject gmGO = GameObject.Find("GameManager");
+        if(gmGO != null) {
+            GameManager gm = gmGO.GetComponent<GameManager>();
+            gm.SetGameMode_AddProps();
+        }
         GameManager.currentProps = g.GetComponent<BuildingObject>();
     }
 }
